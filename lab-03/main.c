8f6e1d5839ee3c3a957352b1cbb7af9d4dad76d7
augustopsalgado <42944134+augustopsalgado@users.noreply.github.com>
2022-05-31 19:50:50 -0300

    Update

--- lab-03/main.c
+++ lab-03/main.c
@@ -1,2 +1,34 @@
-#include<stdlib.h>/* AHB1 Base Addresses ******************************************************/#define STM32_RCC_BASE0x40023800/* 0x40023800-0x40023bff: Resetand Clock control RCC *//* Register Offsets *********************************************************/#define STM32_RCC_AHB1ENR_OFFSET    0x0030/* AHB1 Peripheral Clock enableregister *//* Register Addresses *******************************************************/#define STM32_RCC_AHB1ENR(STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)/* AHB1 Peripheral Clock enable register */#define RCC_AHB1ENR_GPIOCEN(1 << 2)/* Bit 2:  IO port C clockenable */intmain(intargc,char*argv[]){uint32_treg;
+#include <stdlib.h>
+#include <stdint.h>
 
+/* AHB1 Base Addresses ******************************************************/
+
+#define STM32_RCC_BASE              0x40023800      /* 0x40023800-0x40023bff: Reset
+                                                        and Clock control RCC */
+
+/* AHB2 Base Addresses ******************************************************/
+
+#define STM32_GPIOC_BASE            0x48000800U     /* 0x48000800-0x48000bff: GPIO
+                                                        Port C */
+
+/* Register Offsets *********************************************************/
+
+#define STM32_RCC_AHB1ENR_OFFSET    0x0030          /* AHB1 Peripheral Clock enable
+                                                        register */
+
+#define STM32_GPIO_MODER_OFFSET     0x0000          /* GPIO port mode register */
+
+#define STM32_GPIO_OTYPER_OFFSET    0x0004          /* GPIO port output type register */
+
+#define STM32_GPIO_PUPDR_OFFSET     0x000c          /* GPIO port pull-up/pull-down
+                                                        register */
+
+#define STM32_GPIO_BSRR_OFFSET      0x0018          /* GPIO port bit set/reset register */
+
+/* Register Addresses *******************************************************/
+
+#define STM32_RCC_AHB1ENR (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
+#define STM32_GPIOC_MODER (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOC_OTYPER (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOC_PUPDR (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOC_BSRR (STM32_GPIOC_BASE + STM32_GPIO_BSRR_OFFSET)
